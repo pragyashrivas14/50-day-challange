dc9da820abf5d2adbc8e231daae0fcfe3d69ca8c
pragyashrivas <pragyasrivas3@gmail.com>
2025-11-10 23:57:30 +0530

    lec4

---
+++ car.java
@@ -0,0 +1,12 @@
+ class Main {
+    String name;
+    void display(){
+        System.out.println("helloWORLD");
+    }  
+}
+class car{
+    public static void main(String[] args){
+        Main obj=new Main();
+        obj.display();
+    }
+}
