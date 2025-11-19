9c5130e21f7c411f1ced92639570f15736c55020
pragyashrivas <pragyasrivas3@gmail.com>
2025-11-19 19:23:56 +0530

    challange 4

--- array4.c
+++ array4.c
@@ -6,10 +6,21 @@ int main(){
     int arr[n];
     printf("enter element ");
     for(i=0;i<n;i++){
-        scanf("%d",arr[i]);
+        scanf("%d",&arr[i]);
     }
-            for(i=0;i<n;i++){
-    if(arr[i]<0)
+    for(i=0;i<n;i++){
+    if(arr[i]<0){
+        printf(" after rearranging  array %d",arr[i]);
+    }
+}
     
+    for(i=0;i<n;i++){
+    if(arr[i]>0){
+        printf("%d",arr[i]);
+    }
+    
+            }
+
 
 }
+    
